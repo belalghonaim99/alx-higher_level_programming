d2952b0f2293cef71d056c352d3412d828f5ef74
belalghonaim99 <belalghonaim99@gmail.com>
2023-06-07 06:34:54 -0700

    updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for x1 in range(0, 10):
     for x2 in range(x1 + 1, 10):
         if x1 == 8 and x2 == 9:
-                print("{}{}".format(x1, x2))
+                print("{} {}".format(x1, x2))
         else:
-            print("{}{}".format(x1, x2), end=", ")
+            print("{} {}".format(x1, x2), end=", ")
