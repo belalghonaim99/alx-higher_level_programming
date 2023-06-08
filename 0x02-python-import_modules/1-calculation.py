60c6917478e5810871e154dc9e03bc4a765866df
belalghonaim99 <belalghonaim99@gmail.com>
2023-06-08 15:06:06 -0700

    updated

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, mines, divided, multi
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".foramt(a, b, mines(a, b))
-    print("{} * {} = {}".format(a, b, multi(a, b)))
-    print("{} / {} = {}".format(a, b, divided(a, b)))
+    print("{} - {} = {}".foramt(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
