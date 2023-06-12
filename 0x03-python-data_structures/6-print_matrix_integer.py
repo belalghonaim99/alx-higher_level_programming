7a09a38bbbbbc99ab64ebc93775c075a8f12cf10
belalghonaim99 <belalghonaim99@gmail.com>
2023-06-11 20:51:41 -0700

    updated

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        for c in x:
+            print("{:d}".format(c), end="" if c != x[-1] else "")
+            print()
