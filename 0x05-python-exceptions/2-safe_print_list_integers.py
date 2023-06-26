6acc2563a353c7f19539dce6b3e6eee4d05b59d3
belalghonaim99 <belalghonaim99@gmail.com>
2023-06-26 11:31:04 -0700

    updated

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+        r = 0
+    for y in range(x):
+        try:
+            print("{:d}".format(my_list[y]), end="")
+            r += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (r)
