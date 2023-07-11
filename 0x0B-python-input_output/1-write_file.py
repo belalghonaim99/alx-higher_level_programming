47b32b3e54cb58a6a1e4660469c04d3c963494e0
belalghonaim99 <belalghonaim99@gmail.com>
2023-07-11 08:07:32 -0700

    updated

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""define a fun to write a file"""
+"""define file writing fun"""
 
 
 def write_file(filename="", text=""):
     """Write a string to a UTF8 text file.
-        Args: filename (str): name of file to write
-        text (str): text to write the file
-    Returns: The numbers of characters been written """
+
+    Args: filename (str): name of the file written
+        text (str): Text to write the file
+    Returns: the numbers of characters has written"""
     with open(filename, "w", encoding="utf-8") as f:
-         return f.write(text)
+        return f.write(text)
