12a49402135da177a024a90a1ef0e9afea22eafc
belalghonaim99 <belalghonaim99@gmail.com>
2023-07-11 03:22:29 -0700

    updated

--- 0-read_file.py
+++ 0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    """print the content of UTF8 text file to stdout"""
-    with open(filename, encoding="utf-8") as f:
+    
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
