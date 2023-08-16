8e61f95d77bad6842a6b1ef9e5c4e7adb7f46b89
belalghonaim99 <belalghonaim99@gmail.com>
2023-08-15 18:57:01 -0700

    UPDATED task 102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,7 +3,7 @@
 USE hbtn_0c_0
 SELECT city, AVG(value) AS avg_temp 
 FROM temperatures 
-WHERE month IN('july',  'August') 
+WHERE month = July OR month = August
 GROUP BY city 
 ORDER BY avg_temp DESC 
 LIMIT 3;
