77461f2727f4b100f9513f4788371709ec09d0b9
belalghonaim99 <belalghonaim99@gmail.com>
2023-08-15 18:58:42 -0700

    UPDATED task 102 Month Should be INT

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,9 +1,10 @@
 -- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
+-- year INT, month INT, value, INT 
 -- script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
 USE hbtn_0c_0
 SELECT city, AVG(value) AS avg_temp 
 FROM temperatures 
-WHERE month = July OR month = August
+WHERE month = 7 OR month = 8
 GROUP BY city 
 ORDER BY avg_temp DESC 
 LIMIT 3;
