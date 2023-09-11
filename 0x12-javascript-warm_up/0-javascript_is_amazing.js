f697fbcdda33ad8d20ede1743ea0e71ee02e56b6
belalghonaim99 <belalghonaim99@gmail.com>
2023-09-11 12:41:49 -0700

    updated

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// add the javascript is amazing in myVar and print it
+// print javascript is amazing in myVar and print it
 
 const myVar = "JavaScript is amazing";
 console.log(myVar);
