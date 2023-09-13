346eff04682bcab654672f01eb536566c991e0a6
BelalGhonaim <belalpop1999@yahoo.com>
2023-09-13 21:22:20 +0300

    add 4 files

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-module.exports = class Rectangle {};
+module.exports = class Square extends require('./5-square.js') {
+    charPrint (c) {
+      if (c === undefined) {
+        this.print();
+      } else {
+        for (let r = 0; r < this.height; r++) console.log(c.repeat(this.width));
+      }
+    }
+  };
