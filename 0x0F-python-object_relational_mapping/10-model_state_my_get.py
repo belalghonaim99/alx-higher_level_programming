99ff10f7d4c073efa3fa8c567c21733b9fba9277
BelalGhonaim <belalpop1999@yahoo.com>
2023-09-16 23:52:21 +0300

    updated 10-model_state_my_get.py file

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""script that prints the State object with the name
-passed as argument from the database hbtn_0e_6_usa"""
+"""
+script that prints the State object with
+the name passed as argument from the database hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sys import argv
 
 if __name__ == "__main__":
-    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                        pool_pre_ping=True)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Session_maker = sessionmaker(bind=eng)
     sess = Session_maker()
+    Base.metadata.create_all(eng)
 
-    found = False
-    for state in sess.query(State):
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            found = True
-            break
-    if found is False:
+    s = sess.query(State).filter(State.name == argv[4]).first()
+
+    if s:
+        print("{}".format(s.id))
+    else:
         print("Not found")
+    sess.close()
