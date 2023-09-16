61febd2063f35fec101097c39a207bbfe6375199
BelalGhonaim <belalpop1999@yahoo.com>
2023-09-16 14:09:39 +0300

    updated 5-filter file 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-"""script that takes in the name of a state as an argument
-and lists all cities of that state, using the database hbtn_0e_4_us"""
-
-import sys
+"""
+Script that takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa
+"""
 import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
     cr = db.cursor()
-    cr.execute("SELECT * FROM `cities` as `ci` \
-                INNER JOIN `states` as `st` \
-                   ON `ci`.`state_id` = `st`.`id` \
-                ORDER BY `ci`.`id`")
-    print(" , ".join([ci[2] for ci in cr.fetchall() if ci[4] == sys.argv[4]]))
+    cr.execute("SELECT cities.id, cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s", [argv[4]])
+
+    rows = cr.fetchall()
+    y = []
+    for i in rows:
+        y.append(i[1])
+    print(", ".join(y))
+    cr.close()
+    db.close()
