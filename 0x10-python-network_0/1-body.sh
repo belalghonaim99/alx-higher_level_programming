eeac5a67ed5f4218fa31dc4541ee8ab0448f6e13
root <belalghonaim99@yahoo.com>
2023-09-28 16:46:43 +0000

    updated file 0 to merge

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# cript that takes in a URL, sends a GET request to the URL
+# cript that takes in a URL, sends a GET request to the URL..
 if [ $(curl -L -s -X HEAD -w "%{Http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
