42543b09d08c366b74f1da829512c3e83c5b71a1
BelalGhonaim <belalpop1999@yahoo.com>
2023-09-28 19:37:25 +0300

    remove comment

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # cript that takes in a URL, sends a GET request to the URL
-# and displays the body of the response
 if [ $(curl -L -s -X HEAD -w "%{Http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
