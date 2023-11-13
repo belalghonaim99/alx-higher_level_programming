d6d78668ae0a45c6c1be615d1e8670ccb0ce1aaa
BelalGhonaim <belalpop1999@yahoo.com>
2023-11-13 23:33:51 +0200

    add file readme and 0,1,2 files

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+Write a script that prints a message depending of the number of arguments passed:
+
+If no arguments are passed to the script, print “No argument”
+If only one argument is passed to the script, print “Argument found”
+Otherwise, print “Arguments found” */
+if (process.argv.length === 0) {
+    console.log("No argument");
+} else if (process.argv.length === 1){
+    console.log("Argument found");
+} else {
+    console.log("Argument found");
+}
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,2 @@
+# 0x12-javascript-warm_up
+# this my firt task in java-script
