30fc56cc9a1122cc5e1ba3a4e9e639b48f8c977e
BelalGhonaim <belalpop1999@yahoo.com>
2023-11-14 00:09:28 +0200

    updated file 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,10 +5,11 @@ Write a script that prints a message depending of the number of arguments passed
 If no arguments are passed to the script, print “No argument”
 If only one argument is passed to the script, print “Argument found”
 Otherwise, print “Arguments found” */
-if (process.argv.length === 0) {
+const number = process.argv.length - 2;
+if (number === 0) {
     console.log('No argument');
-} else if (process.argv.length === 1){
+} else if (number === 1) {
     console.log('Argument found');
 } else {
-    console.log('Argument found');
+    console.log('Arguments found');
 }
