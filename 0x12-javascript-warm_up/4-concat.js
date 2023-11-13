20f3af56a7417cc8458f229b2f315fdbaefa6747
belalghonaim99 <belalpop1999@yahoo.com>
2023-11-13 15:34:35 -0800

    change the 4=concat file

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,4 +4,4 @@ Write a script that prints two arguments passed to it, in the following format: 
 You must use console.log(...) to print all output
 You are not allowed to use var
 */
-console.log('${process.argv[2]} is ${process.argv[3]}');
+console.log(process.argv[2] + ' is ' + process.argv[3]);
