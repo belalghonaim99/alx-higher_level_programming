e68ce864c30cc58a3711b03bf6b4160482520247
belalghonaim99 <belalpop1999@yahoo.com>
2023-11-13 14:35:43 -0800

    updated

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,7 +5,7 @@ If no arguments are passed to the script, print “No argument”
 You must use console.log(...) to print all output
 */
 if (process.argv[2]) {
-    console.log(process.argv[2]);
+  console.log(process.argv[2]);
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
