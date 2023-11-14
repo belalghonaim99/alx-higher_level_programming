fa6bd9fe6f4b1d3ea78d8b944ebafb5a6b4dd8b2
belalghonaim99 <belalpop1999@yahoo.com>
2023-11-13 16:22:27 -0800

    updated

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-Write a script that searches the second biggest integer in the list of arguments.
+Write a script that searches the second bigger
 You can assume all arguments can be converted to integer
 If no argument passed, print 0
 If the number of arguments is 1, print 0
