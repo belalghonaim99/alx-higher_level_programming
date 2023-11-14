3ca8500deb31a93f5a1daee144e266c95bea14df
BelalGhonaim <belalpop1999@yahoo.com>
2023-11-14 02:24:09 +0200

    updateda agian

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,8 +6,8 @@ If no argument passed, print 0
 If the number of arguments is 1, print 0
 */
 if (process.argv.length <= 3) {
-    console.log(0);
+  console.log(0);
 } else {
     const list = process.argv.sort();
-    console.log(list.reverse()[1]);
+  console.log(list.reverse()[1]);
 }
