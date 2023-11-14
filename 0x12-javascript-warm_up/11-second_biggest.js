de2d62e8ec5272b760f4f2e2f74f5a4a2dea866e
BelalGhonaim <belalpop1999@yahoo.com>
2023-11-14 02:07:56 +0200

    updatd

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,6 +8,6 @@ If the number of arguments is 1, print 0
 if (process.argv.length <= 3) {
     console.log(0);
 } else {
-    const list = process.argv.sort();
-    console.log(list.reverse()[1]);
+    const array = process.argv.sort();
+    console.log(array.reverse()[1]);
 }
