a8fdc9c186e3d8a43312c685efd7edd3408a17ae
belalghonaim99 <belalpop1999@yahoo.com>
2023-11-17 23:04:26 -0800

    updated all files pycodestyle

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Write a script that lists all State objects that contain
 the letter a from the database hbtn_0e_6_usa
-Your script should take 3 arguments: mysql username,
+Your script should take 3 arguments: mysql username
 mysql password and database name
 You must use the module SQLAlchemy
-You must import State and Base from model_state - from model_state import Base, State
+You must import State and Base from model_state
+from model_state import Base, State
 Results must be sorted in ascending order by states.id"""
 import sys
 from sqlalchemy import create_engine
@@ -13,8 +14,8 @@ from model_state import State
 
 if __name__ == "__main__":
     e = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                        pool_pre_ping=True)
+                      .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                      pool_pre_ping=True)
     Session_maker = sessionmaker(bind=e)
     s = Session_maker()
 
