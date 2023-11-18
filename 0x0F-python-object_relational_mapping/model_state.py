4a77cdd62f36c42623e416e196bf42a6cedb988c
BelalGhonaim <belalpop1999@yahoo.com>
2023-11-18 07:51:14 +0200

    add mode_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
 Write a python file that contains the class definition
-of a State and an instance Base = declarative_base():
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+of a State and an instance Base = declarative_base():"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
