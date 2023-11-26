fc9fd0fc067dbf8e94b9ab1ce6912b320eca1372
BelalGhonaim <belalpop1999@yahoo.com>
2023-11-26 04:32:35 +0200

    add 4 files and readme file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Your SSH client configuration
+# must be configured to use the private key ~/.ssh/school
+
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
