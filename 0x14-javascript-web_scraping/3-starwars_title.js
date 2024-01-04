f6b8af1b15eee91b2c0f719b41779d36a6e50897
BelalGhonaim <belalpop1999@yahoo.com>
2024-01-04 23:59:27 +0200

    add file 2&3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// prints the title of a Star Wars movie where
+// the episode number matches a given integer.
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  }
+});
