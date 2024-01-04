dc21c4170ef313bd9e7949e6917868f9e366d366
BelalGhonaim <belalpop1999@yahoo.com>
2024-01-05 01:44:59 +0200

    fixed file 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,14 +5,10 @@ const request = require('request');
 request(process.argv[2], (error, response, body) => {
   if (!error) {
     const movies = JSON.parse(body).results;
-    const cwwa = movies.reduce((count, movie) => {
+    console.log(results.reduce((count, movie) => {
       return movie.characters.some((character) => character.endsWith('/18/')) 
       ? count + 1
       : count;
-    }, 0);
-
-    console.log(cwwa);
-  } else {
-    console.error(error);
+    }, 0));
   }
 });
